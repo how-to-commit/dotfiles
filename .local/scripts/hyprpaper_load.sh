89ae99fdd081829b0a25e7ba94801c766b346e52
JL <52496366+how-to-commit@users.noreply.github.com>
2025-05-11 22:57:39 +0800

    feat(wp): new wp, change script to not be annoying

--- .local/scripts/hyprpaper_load.sh
+++ .local/scripts/hyprpaper_load.sh
@@ -1,14 +1,16 @@
 #!/bin/sh
 # we need to set the wallpaper inside the config file to ensure that it starts at startup.
 
-# start hyprpaper here
-hyprpaper &
+# start hyprpaper here if not already started
+if ! pidof -x "hyprpaper" >/dev/null; then 
+    echo Starting hyprpaper
+    hyprpaper &
+else
+    echo hyprpaper already started, continuing
+fi
 
-# shuffle through the wallpapers
+# get a new one on boot
 WALLPAPER_DIR="$HOME/Pictures/Wallpapers/"
-while true; do
-    CURRENT_WALLPAPER=$(hyprctl hyprpaper listloaded)
-    NEW=$(find "$WALLPAPER_DIR" -type f ! -name "$(basename "$CURRENT_WALL")" | shuf -n 1)
-    hyprctl hyprpaper reload ,"$NEW"
-    sleep 10m
-done
+CURRENT_WALLPAPER=$(hyprctl hyprpaper listloaded)
+NEW=$(find "$WALLPAPER_DIR" -type f | shuf -n 1)
+hyprctl hyprpaper reload ,"$NEW"

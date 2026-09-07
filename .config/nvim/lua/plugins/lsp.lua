f5ed26167e3d621221c143ade9b1fcf53ba94672
JL <52496366+how-to-commit@users.noreply.github.com>
2026-09-07 11:41:29 +0800

    chore(nvim): stop `tsc` from formatting

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -1,18 +1,25 @@
 return {
-    { 'mason-org/mason.nvim', opts = {} },
+	{ "mason-org/mason.nvim", opts = {} },
 
-    {
-        'mason-org/mason-lspconfig.nvim',
-        dependencies = { 'neovim/nvim-lspconfig' },
-        opts = {}
-    },
+	{
+		"mason-org/mason-lspconfig.nvim",
+		dependencies = { "neovim/nvim-lspconfig" },
+		opts = {},
+	},
 
-    {
-        "neovim/nvim-lspconfig",
-        config = function()
-            vim.diagnostic.config({
-                virtual_text = true
-            })
-        end
-    }
+	{
+		"neovim/nvim-lspconfig",
+		config = function()
+			vim.diagnostic.config({
+				virtual_text = true,
+			})
+
+			vim.lsp.config("tsc", {
+				on_attach = function(client, _)
+					client.server_capabilities.documentFormattingProvider = false
+					client.server_capabilities.documentRangeFormattingProvider = false
+				end,
+			})
+		end,
+	},
 }
